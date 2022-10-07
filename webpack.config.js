e85c4b2479a756ecf2b2117dc52508ee6857fd9a
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-10-07 16:37:36 +0200

    Setting webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do List',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+   // publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+ 
+};
